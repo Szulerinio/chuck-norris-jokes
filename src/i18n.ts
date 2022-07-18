6ab11367285eeaa707e9058ef65e5a763c78554c
Szymon Kozłowski <szymonkozlowski48@gmail.com>
2022-07-18 13:42:31 +0200

    Add react-i18next

---
+++ src/i18n.ts
@@ -0,0 +1,24 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+const resources = {
+  en: {
+    translation: {
+      "Save joke_one": "Save Joke",
+      "Save joke_other": "Save Jokes",
+      Impersonate: "Impersonate Chuck Norris",
+      categories: "Categories",
+      "Select category": "Select category",
+    },
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
