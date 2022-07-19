f9d033fd631289ba828e6be2d96458269f610dc2
Szymon Kozłowski <szymonkozlowski48@gmail.com>
2022-07-19 12:34:17 +0200

    move localization to JSON file

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,15 +1,10 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import * as translationEN from "./locales/en/translation.json";
 
 export const resources = {
   en: {
-    translation: {
-      saveJoke_one: "Save a Joke",
-      saveJoke_other: "Save {{count}} Jokes",
-      impersonate: "Impersonate Chuck Norris",
-      categories: "Categories",
-      selectCategory: "Select category",
-    },
+    mainPage: translationEN,
   },
 };
 
